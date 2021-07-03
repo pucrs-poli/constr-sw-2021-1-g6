e3b490a2502eb39f27dc10ef00673b77d5b5a1c4
guicavalheiro <cavalheiro.dev@gmail.com>
2021-07-03 17:37:23 -0300

    app

--- src/database/index.js
+++ src/database/index.js
@@ -5,7 +5,8 @@ mongoose.Promise = global.Promise;
 
 try {
     // console.log("Logging into: " + process.env.MONGODB_URL);
-    mongoose.connect("mongodb+srv://group6:cvKZXhQST0r0pGGO@group6.5txl5.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", {
+    //mongodb+srv://atlas:R9v47d6q4pjRAfdn@group6.5txl5.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
+    mongoose.connect("mongodb+srv://atlas:123123123@atlas.5txl5.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", {
     //mongoose.connect('mongodb://localhost/noderest', {
         useNewUrlParser: true,
         useUnifiedTopology: true,
